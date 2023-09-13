394b358b4bd72c2bafa9034c33501ed85d295a5a
Thorsten Marx <t.marx@eggheads.de>
2023-09-13 16:24:07 +0200

    v1.0.0

--- src/main/java/org/lumongo/storage/constants/MongoConstants.java
+++ src/main/java/org/lumongo/storage/constants/MongoConstants.java
@@ -1,5 +1,25 @@
 package org.lumongo.storage.constants;
 
+/*-
+ * #%L
+ * mongo-directory
+ * %%
+ * Copyright (C) 2023 Marx-Software
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 public interface MongoConstants {
 	public static interface StandardDBs {
 		public static final String ADMIN = "admin";
